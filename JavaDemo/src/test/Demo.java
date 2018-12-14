c45843612b7a20bee22ea11d613b3dee9d1b47dc
ChangeMakeBetter <yangxiaohua321.hi@163.com>
2018-12-14 14:58:15 +0800

    建了个java项目

---
+++ JavaDemo/src/test/Demo.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * TestUtils<br> Created by yangxiaohua on 2018/12/14.
+ */
+public class Demo {
+  public static void main(String[] args) {
+    System.out.println("demo test");
+  }
+}
